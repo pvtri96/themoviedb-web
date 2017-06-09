121990c8cf2a72728a8703a7e213fb8a1e8eb61b
Tri Pham <pvtri96@gmail.com>
2017-06-09 16:04:10 +0700

    Init sass support

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,23 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import stylesheet from 'styles/index.scss'
+// or, if you work with plain css
+// import stylesheet from 'styles/index.css'
+
 
 const Index = props => {
     return (
+      <div>
+        <style dangerouslySetInnerHTML={{ __html: stylesheet }} />
         <div>
             Hello NextJS
         </div>
+      </div>
     );
 };
 
 Index.propTypes = {
-    
+
 };
 
 export default Index;
