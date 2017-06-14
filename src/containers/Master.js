4c97dffac8adc3eb81aebeaec25958d496af877d
ManhLe <lequocmanh2010@gmail.com>
2017-06-14 09:15:13 +0700

    change branch to manh/feature/movie

--- src/containers/Master.js
+++ src/containers/Master.js
@@ -4,7 +4,7 @@ import Header from './Header';
 import Footer from './Footer';
 
 //Import stylesheet
-import vendorsheet from 'styles/vendor.scss';
+// import vendorsheet from 'styles/vendor.scss';
 // import mastersheet from './MasterLayout.scss';
 // If you dont have scss, just import css.
 // import vendorsheet from 'styles/vendor.css';
@@ -15,7 +15,6 @@ const Master = (props) => (
     {/* Customize head tag of the page */}
     <Head>
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-      <style dangerouslySetInnerHTML={{ __html: vendorsheet }} />
       {/* Import Global stylesheet */}
       <title>The Movie DB</title>
     </Head>
