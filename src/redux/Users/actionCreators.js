e15730cc8099b818c5b0c4a41e986ad1254d51c8
Tri Pham <pvtri96@gmail.com>
2017-06-14 23:42:17 +0700

    change USER_FETCH to USER_FETCH_REQUESTED

--- src/redux/Users/actionCreators.js
+++ src/redux/Users/actionCreators.js
@@ -4,7 +4,7 @@ import actionTypes from './actionTypes';
 const API_URL = 'https://jsonplaceholder.typicode.com/users';
 
 // action creators
-const usersFetch = () => ({ type: actionTypes.USERS_FETCH });
+const usersFetchRequested = () => ({ type: actionTypes.USERS_FETCH_REQUESTED });
 const usersFetchFulfilled = (users) => ({
   type: actionTypes.USERS_FETCH_FULFILLED,
   payload: users
@@ -19,7 +19,7 @@ const usersFetchRejected = (err) => ({
 export const fetchUsers = () => async (dispatch) => {
   //console.log(dispatch);
   //Dispath user fetch
-  dispatch(usersFetch());
+  dispatch(usersFetchRequested());
   try {
     const users = await axios.get(API_URL).then(response => response.data);
     return dispatch(usersFetchFulfilled(users))
