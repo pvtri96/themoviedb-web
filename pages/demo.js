c1779afc258127cdbed5e96384fc486662524b1b
ManhLe <lequocmanh2010@gmail.com>
2017-06-16 10:41:24 +0700

    merge from master

--- pages/demo.js
+++ pages/demo.js
@@ -6,28 +6,25 @@ import { getStore } from '../src/redux';
 import { usersActions, usersSelector } from '../src/redux/users';
 
 class Index extends Component {
-  // static getInitialProps ({ store }) {
-  //   store.dispatch(usersActions.fetchUsers());
-  // }
-
+  static async getInitialProps ({ store }) {
+    await store.dispatch(usersActions.fetchUsers());
+  }
   render() {
     return (
-      <div>
-        <Master>
-          <div>
-            <button onClick={ this.props.fetchUsers }>
-              Fetch users
-            </button>
-            <h1>
-              { this.props.users.map(({id, name}) => (
-                <div key={id}>
-                  {name}
-                </div>
-              )) }
-            </h1>
-          </div>
-        </Master>
-      </div>
+      <Master>
+        <div>
+          <button onClick={ this.props.fetchUsers }>
+            Fetch users
+          </button>
+          <h1>
+            { this.props.users.map(({id, name}) => (
+              <div key={id}>
+                {name}
+              </div>
+            )) }
+          </h1>
+        </div>
+      </Master>
     );
   }
 }
--- src/redux/index.js
+++ src/redux/index.js
@@ -2,12 +2,17 @@ import { createStore, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import thunkMiddleware from 'redux-thunk';
 import rootReducer from './rootReducer';
+import { USERS_INITIAL_STATE, USERS_KEY } from './users';
+
+const INITIAL_STATE = {
+  [USERS_KEY]: USERS_INITIAL_STATE
+};
 
 const middleware = applyMiddleware(thunkMiddleware);
 
-const store = createStore(rootReducer, composeWithDevTools(middleware));
-
-export const getStore = () => store;
+export const getStore = (initialState = INITIAL_STATE) => (
+  createStore(rootReducer, initialState, composeWithDevTools(middleware))
+);
 
 export default {
   getStore
